a2e2f90f2520e0d71ab4c443844964d28c3e3acd
atheeq01 <mhdatheeq0@gmail.com>
2025-09-24 11:36:31 +0530

    basic setup frontend,backend

---
+++ breezy-media-backend/src/main/java/com/breezy/breezy_media_backend/BreezyMediaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.breezy.breezy_media_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreezyMediaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreezyMediaBackendApplication.class, args);
+	}
+
+}
---
+++ breezy-media-backend/src/test/java/com/breezy/breezy_media_backend/BreezyMediaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breezy.breezy_media_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreezyMediaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
